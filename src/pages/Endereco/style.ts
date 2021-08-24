9eba47d2a4d3bdd914e689c7be6dc4e8853f0043
daniggie <dani.gross53@gmail.com>
2021-08-24 16:49:46 -0300

    24/08 ajustes

--- src/pages/Endereco/style.ts
+++ src/pages/Endereco/style.ts
@@ -1,8 +1,18 @@
 import styled, { css } from 'styled-components';
 
-export const Corpo = styled.h1`
+export const Corpo = styled.div`
+width: 750px;
+margin-top: 40px;
+background-color: #000;
+border-radius: 5px;
+padding: 15px 15px 15px 15px;
+
+h1{
   color: #fff;
-  font-size: 25px;
-  font-family: sans-serif;
-  max-width: 450px;
+}
+
+p{
+  font-size: 15px;
+  color: #fff;
+}
 `;
